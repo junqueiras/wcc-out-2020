5ab355a0b21e943a115318f2f4aac5184b340373
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-01 14:09:59 -0300

    Adding Room LearnedItem entity

--- Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/entities/LearnedItem.kt
+++ Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/entities/LearnedItem.kt
@@ -1,7 +1,18 @@
 package com.wcc.whatdoilearn.entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "learned_item")
 data class LearnedItem(
+        @ColumnInfo(name = "item_title")
         val tittle: String,
+        @ColumnInfo(name = "item_description")
         val description: String,
-        val understandingLevel: UnderstandingLevel
+        @ColumnInfo(name = "item_level")
+        val understandingLevel: UnderstandingLevel,
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "item_id")
+        val id: Int = 0
 )
