e9104bf329dc63216a137e9c762d8966ae038bc1
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-01 21:10:35 -0300

    Adding new item activity

--- Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/view/MainActivity.kt
+++ Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.wcc.whatdoilearn.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
@@ -20,5 +21,10 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter = adapter
         adapter.data = learnedItems
+
+        binding.newItemButton.setOnClickListener {
+            val intent = Intent(this, NewItemActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
