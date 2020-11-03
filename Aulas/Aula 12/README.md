e8bc6b9475454615a4872057d0bebb1dc63b8bbe
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-03 11:00:29 -0300

    Adding class 11 and 12 files

---
+++ Aulas/Aula 12/README.md
@@ -0,0 +1,19 @@
+# Aula 12
+
+## Objetivo:
+Objetivo: Entender mecanisnmo View Binding, compreender guia de arquitetura android e seus fundamentos: MVVM, Architecture components. Implementar um banco de dados a partir do Room.
+
+
+## Informações extras:
+
+### Links extras
+- [View Binding](https://youtu.be/W7uujFrljW0)
+- [Guia de arquitetura android](https://developer.android.com/jetpack/docs/guide)
+- [Room](https://www.youtube.com/watch?v=SKWh4ckvFPM)
+
+### Palavras chave
+- Recycler view
+- Adapter pattern
+- View Holder
+
+
--- Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/view/MainActivity.kt
+++ Projetos/whatdidilearn/app/src/main/java/com/wcc/whatdoilearn/view/MainActivity.kt
@@ -2,33 +2,39 @@ package com.wcc.whatdoilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wcc.whatdoilearn.data.LearnedItemDao
 import com.wcc.whatdoilearn.data.LearnedItemsDatabase
 import com.wcc.whatdoilearn.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var learnedItemsDao: LearnedItemDao
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val recyclerView = binding.itemsLearnedRecyclerView
-        val scope = CoroutineScope(Dispatchers.IO)
-        val database = LearnedItemsDatabase.getDatabase(this, scope)
-        val learnedItemDao = database.learnedItemDao()
+        val scopeDB = CoroutineScope(Dispatchers.IO)
+        val database = LearnedItemsDatabase.getDatabase(this, scopeDB)
+        learnedItemsDao = database.learnedItemDao()
 
-        val adapter = LearnedItemsAdapter()
-        recyclerView.adapter = adapter
+        recyclerView.adapter = LearnedItemsAdapter()
 
-        scope.launch {
-            val learnedItems = learnedItemDao.getAll()
-            adapter.data = learnedItems
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val scopeMain = CoroutineScope(Dispatchers.Main)
+        scopeMain.launch {
+            val learnedItems = learnedItemsDao.getAll()
+            (binding.itemsLearnedRecyclerView.adapter as LearnedItemsAdapter).data = learnedItems
         }
-
-
     }
 }
