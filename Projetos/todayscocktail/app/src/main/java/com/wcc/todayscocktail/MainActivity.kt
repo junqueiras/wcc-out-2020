47ca7de0aea1c295e9665c09a5847d7aeb06ca23
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-06 17:29:46 -0300

    adding glide

--- Projetos/todayscocktail/app/src/main/java/com/wcc/todayscocktail/MainActivity.kt
+++ Projetos/todayscocktail/app/src/main/java/com/wcc/todayscocktail/MainActivity.kt
@@ -2,9 +2,11 @@ package com.wcc.todayscocktail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.wcc.todayscocktail.repository.CocktailsListRepository
 import com.wcc.todayscocktail.viewmodel.CocktailsListViewModel
 import com.wcc.todayscocktail.viewmodel.CocktailsListViewModelFactory
@@ -18,8 +20,14 @@ class MainActivity : AppCompatActivity() {
         val viewModel = ViewModelProvider(this, viewModelFactory).get(CocktailsListViewModel::class.java)
         val list = viewModel.cocktailList
 
+
+
         list.observe(this, Observer {
-            findViewById<TextView>(R.id.textView).text = it[0].name
+            val imageView = findViewById<ImageView>(R.id.imageView);
+            Glide.with(this)
+                    .load(it[0].thumbUrl)
+                    .into(imageView)
+            findViewById<TextView>(R.id.textView).text = it[0].thumbUrl
         })
     }
 }
