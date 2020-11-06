c85d2d41613ea4768295ca55c22f8b5cbc2d44e7
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-06 09:46:18 -0300

    Creating retrofit base structure

---
+++ Projetos/todayscocktail/app/src/main/java/com/wcc/todayscocktail/network/Cocktail.kt
@@ -0,0 +1,12 @@
+package com.wcc.todayscocktail.network
+
+import com.squareup.moshi.Json
+
+data class Cocktail(
+    @Json(name = "idDrink")
+    val id: Int,
+    @Json(name = "strDrink")
+    val name: String,
+    @Json(name = "strDrinkThumb")
+    val thumbUrl: String
+)
