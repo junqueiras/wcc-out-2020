950dba1fa9aa2a4f94cc242be5934306cdb2530b
Marcella Souza <mlsouza@thoughtworks.com>
2020-11-07 08:55:53 -0300

    Adding recycler view

--- Projetos/todayscocktail/app/src/main/java/com/wcc/todayscocktail/view/MainActivity.kt
+++ Projetos/todayscocktail/app/src/main/java/com/wcc/todayscocktail/view/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.wcc.todayscocktail
+package com.wcc.todayscocktail.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
+import androidx.recyclerview.widget.RecyclerView
+import com.wcc.todayscocktail.R
+import com.wcc.todayscocktail.network.Cocktail
 import com.wcc.todayscocktail.repository.CocktailsListRepository
 import com.wcc.todayscocktail.viewmodel.CocktailsListViewModel
 import com.wcc.todayscocktail.viewmodel.CocktailsListViewModelFactory
@@ -20,14 +21,12 @@ class MainActivity : AppCompatActivity() {
         val viewModel = ViewModelProvider(this, viewModelFactory).get(CocktailsListViewModel::class.java)
         val list = viewModel.cocktailList
 
-
+        val recyclerView = findViewById<RecyclerView>(R.id.cocktailsListRecyclerView)
+        val adapter = CocktailsListAdapter()
+        recyclerView.adapter = adapter
 
         list.observe(this, Observer {
-            val imageView = findViewById<ImageView>(R.id.imageView);
-            Glide.with(this)
-                    .load(it[0].thumbUrl)
-                    .into(imageView)
-            findViewById<TextView>(R.id.textView).text = it[0].thumbUrl
+            adapter.data = it
         })
     }
 }
